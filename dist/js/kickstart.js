8995b91701e3a067eae099eb65dcdc09f3fd8963
Cheton Wu (RD-TW) <cheton_wu@trend.com.tw>
2015-03-12 03:15:28 -0700

    Update dist files

--- dist/js/kickstart.js
+++ dist/js/kickstart.js
@@ -21,7 +21,7 @@
     // Save the previous value of the 'Kickstart' variable, so that it can be restored later on, if 'noConflict' is used.
     var _Kickstart = root.Kickstart;
     // Current version of the library. Keep in sync with 'package.json'.
-    Kickstart.VERSION = "__VERSION__";
+    Kickstart.VERSION = "1.1.0";
     // Runs in *noConflict* mode, returning the `Kickstart` variable to its previous owner.
     // Returns a reference to this object.
     Kickstart.noConflict = function() {
